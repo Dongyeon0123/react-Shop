9c4a788242dc9e4c222453716d9a7c0172b9983f
dongyeon <ldy1951123@gmail.com>
2025-04-06 01:40:47 +0900

    useParams로 여러 페이지 만드는 법

--- src/App.jsx
+++ src/App.jsx
@@ -41,17 +41,25 @@ function App() {
 
           </div>
           <div className="box">
-            <Shoes shoes={shoes} />
+            <Card shoes={shoes[0]} i={1} />
+            <Card shoes={shoes[1]} i={2} />
+            <Card shoes={shoes[2]} i={3} />
           </div>
           </>
         } />
-        <Route path="/detail" element={<Detail/>} />
+        <Route path="/detail/:id" element={<Detail shoes={shoes}/>} />
+        {/* 페이지 여러개 만들고 싶으면, :URL 파라미터 사용 */}
 
         <Route path='/about' element={<About/>}>
           <Route path='member' element={<div>멤버임</div>} />
           <Route path='location' element={<About/>}/>
         </Route>
         {/* 위에꺼는 nested routes라고 함. */}
+
+        <Route path='/event' element={<Event/>}>
+          <Route path='one' element={<div>첫 주문 시, 양배추즙 서비스</div>}></Route>
+          <Route path='two' element={<div>생일기념 쿠폰받기</div>}></Route>
+        </Route>
 
 
         {/* 라우터로 페이지 나누는 법 */}
@@ -68,6 +76,25 @@ function About() {
       <h4>회사정보</h4>
       <Outlet></Outlet>
       
+    </div>
+  )
+}
+
+function Event() {
+  return (
+    <div className='event-main'>
+      <h4>오늘의 이벤트</h4>
+      <Outlet></Outlet>
+    </div>
+  )
+}
+
+function Card(props){
+  return (
+    <div className="col-md-4">
+      <img src={'https://codingapple1.github.io/shop/shoes' + props.i + '.jpg'} width="80%" />
+      <h4>{ props.shoes.title }</h4>
+      <p>{ props.shoes.price }</p>
     </div>
   )
 }
--- src/pages/detail.jsx
+++ src/pages/detail.jsx
@@ -1,20 +1,41 @@
-function Detail() {
-    return(
+import { useParams } from "react-router-dom";
+
+function Detail(props) {
+  let { id } = useParams();
+
+  // id 값과 일치하는 상품을 배열에서 찾기
+  let foundShoe = props.shoes.find((item) => item.id == id);
+
+  // 상품이 없는 경우 예외 처리
+  if (!foundShoe) {
+    return (
       <div className="container">
         <div className="row">
-          <div className="col-md-6">
-            <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
-          </div>
-          <div className="col-md-6">
-            <h4 className="pt-5">상품명</h4>
-            <p>상품설명</p>
-            <p>120000원</p>
-            <button className="btn btn-danger">주문하기</button>
-            {/* 이거 컴포넌트로 빼보기. */}
+          <div className="col text-center pt-5">
+            <h4>❌ 해당 상품을 찾을 수 없습니다.</h4>
+            <p>다시 확인해주세요.</p>
           </div>
         </div>
       </div>
-    )
+    );
+  }
+
+  // 상품이 있는 경우 정상 출력
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-md-6">
+          <img src={`https://codingapple1.github.io/shop/shoes${parseInt(id)+1}.jpg`} width="100%" />
+        </div>
+        <div className="col-md-6">
+          <h4 className="pt-5">{foundShoe.title}</h4>
+          <p>{foundShoe.content}</p>
+          <p>{foundShoe.price}원</p>
+          <button className="btn btn-danger">주문하기</button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Detail;
